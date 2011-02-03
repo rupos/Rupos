7db20f046a1a6b8ad0488306c26e53092481a634
Roberto Guanciale <guancio@gmail.com>
2011-02-03 11:26:54 +0100

    Cambiato il generator per usare timestamp veri

--- prom5_log_files/generator.py
+++ prom5_log_files/generator.py
@@ -1,27 +1,30 @@
 #!/usr/bin/python
 import random
+import time
 
 class Entry:
     def __init__(self, name):
         self.name = name
     def gen(self, t):
-        delta1 = random.randint(1, 30)
-        delta2 = random.randint(delta1, 59)
+        delta1 = random.randint(1, 30*60)
+        delta2 = random.randint(delta1+1, 59*60)
+        t0 = time.strftime("%Y-%m-%dT%H:%M:%S.000+01:00", time.gmtime(t+delta1))
+        t1 = time.strftime("%Y-%m-%dT%H:%M:%S.000+01:00", time.gmtime(t+delta2))
         res = """
       <AuditTrailEntry>
 	<WorkflowModelElement>%s</WorkflowModelElement>
 	<EventType >start</EventType>
-	<Timestamp>1970-01-%.2dT12:%.2d:00.000+01:00</Timestamp>
+	<Timestamp>%s</Timestamp>
 	<Originator>Guancio</Originator>
       </AuditTrailEntry>
       <AuditTrailEntry>
 	<WorkflowModelElement>%s</WorkflowModelElement>
 	<EventType >complete</EventType>
-	<Timestamp>1970-01-%.2dT12:%.2d:00.000+01:00</Timestamp>
+	<Timestamp>%s</Timestamp>
 	<Originator>Guancio</Originator>
       </AuditTrailEntry>
-        """ % (self.name, t, delta1, self.name, t, delta2)
-        return (res, t+1)
+        """ % (self.name, t0, self.name, t1)
+        return (res, t+60*60)
 
 class Recursion:
     def __init__(self, e):
@@ -29,10 +32,24 @@ class Recursion:
     def gen(self, t):
         res = ""
         p = 0.5
+        max_rec = 2
+        it = 1
         while random.random() < p:
+            if max_rec is not None and it > max_rec:
+                break
             (res1, t) = self.e.gen(t)
             res += res1
+            it += 1
         return (res, t)
+
+class Choice:
+    def __init__(self, l):
+        self.l = l
+    def gen(self, t):
+        res = ""
+        x = random.randint(0, len(self.l)-1)
+        (res1, t1) = self.l[x].gen(t)
+        return (res1, t1)
 
 class Par:
     def __init__(self, l):
@@ -51,55 +68,70 @@ class Par:
         return (res, t1)
 
 class Sequence:
-    def __init__(self, l):
+    def __init__(self, l, p=0):
         self.l = l
+        self.p = p
     def gen(self, t):
         res = ""
         exceptions = [Entry("Draft")]#, Entry("Study")]
-        #p = 0.10
-        p = 0.0
         for a in self.l:
             (res1, t) = a.gen(t)
             res += res1
-            if random.random() < p:
+            if random.random() < self.p:
                 a1 = exceptions[random.randint(0, len(exceptions) - 1)]
                 (res1, t) =a1.gen(t)
                 res += res1
         return (res, t)
         
 
-def gen_sequence():
-    t = 0
+def gen_sequence(t):
     acts = Sequence([
             Entry("RegisterUser"),
             # Par([
-            #        Entry("Draft"), 
-                    # Entry("Study")
-                # ]),
-            Recursion(
-                 Sequence([
-            #         Entry("Restart"),
-#                     Par([
-                                 Entry("Draft"),
-#                                 Entry("Study")
-                                 ]),
-            #         ])
-                 ), 
+                    Entry("Draft"), 
+            #         Entry("Study")
+            #         ]),
+            # Recursion(
+            #       # Sequence([
+            #       #       Entry("Restart"),
+            #             Par([
+            #                     Entry("Draft"), 
+            #                     Entry("Study")
+            #                     ])
+            #             # ])
+            #       )
+            # ,
+#             Recursion(
+#                  Sequence([
+#             #         Entry("Restart"),
+# #                     Par([
+#                                  Entry("Draft"),
+# #                                 Entry("Study")
+#                                  ]),
+#             #         ])
+#                  ),
+Sequence([ 
             Entry("Review"), 
-            Entry("Published")])
+             # Choice([
+                    Entry("Published")
+                    # ,Entry("Rejected")
+                    # ])
+], p=0.1)
+            ])
 
     exceptions = ["Draft"]
 
     p = 0.00
 
-    return acts.gen(0)[0]
+    return acts.gen(t)[0]
 
 def gen_log(i):
+    t = time.time() + 3600 * i
     return """
     <ProcessInstance id="%i" description="PaperOne">
       %s
     </ProcessInstance>
-""" % (i, gen_sequence())
+""" % (i, gen_sequence(t))
 
 
 print("""<?xml version="1.0" encoding="UTF-8" ?>
@@ -108,7 +140,7 @@ print("""<?xml version="1.0" encoding="UTF-8" ?>
   <Process id="Paper" description="Paper Submission">
 """)
 
-for i in range(5):
+for i in range(100):
     print(gen_log(i))
 
 print("""
