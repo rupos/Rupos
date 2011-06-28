e72b83d2efa7d6889af1bb9cc1bc5b592bfdec5f
Roberto Guanciale <guancio@gmail.com>
2011-06-28 10:51:14 +0200

    Aggiunto script per scaricare prom

---
+++ download_prom6.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+import os
+import sys
+
+if len(sys.argv) < 2:
+    print "script.py <directory>"
+    exit(1)
+
+files = os.popen2("svn list https://svn.win.tue.nl/repos/prom/Packages/")[1].read().split("\n")
+files = [f.strip() for f in files if f.strip()!=""]
+
+destDir = sys.argv[1] 
+os.mkdir(destDir)
+
+i = 0
+for f in files:
+    os.popen2("svn co --ignore-externals https://svn.win.tue.nl/repos/prom/Packages/"+f + "/Trunk " + destDir + "/" + f)[1].read()
+    i += 1
+    print "Downloaded ", f, "%d/%d"%(i, len(files))
